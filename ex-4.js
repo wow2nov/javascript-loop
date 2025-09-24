9815434488258f89d79cbb717c1d37d993b7387e
Supapond <supapondmaliwab@gmail.com>
2025-09-24 16:25:36 +0700

    fead:ex-4 add code restaurantnemes

--- ex-4.js
+++ ex-4.js
@@ -3,4 +3,12 @@ let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
 
-console.log(newRestaurants);
+for(let i=0; i < restaurants.length; i++){
+   newRestaurants.push(restaurants[i]);
+   //นำเอาที่วนจากบบรทัดบนมาpush(ใส่)ใน newRestaurants
+    
+   console.log(`Restaurant Name: ${restaurants[i]}`); 
+   //ให้นำ console.log ไปอยู่ในลูปแทนจะทำให้ผลลัพธ์เรียงทุกชื่อ
+};
+
+
